f91757e3725870a353669091c856c7f2640efe83
YSK <sunil.kumarya2@gmail.com>
2016-05-01 17:04:39 +0000

    Auth Scaffolds

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">What you have To Do :)</div>
+                <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    Use our app to list out what TO DO
+                    You are logged in!
                 </div>
             </div>
         </div>
